fdc49eca0ff4a7e2f8d56cfda916df0d37b4ca80
Igor Suchilov <59040634+igorsuchilov@users.noreply.github.com>
2021-05-09 14:32:19 +0200

    Delete internet_gateway.tf

--- internet_gateway.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "ig" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name        = format("%s, %s!",var.internet_gateway_name,var.environment)
-    Environment = var.environment
-  }
-}
