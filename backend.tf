a6e808232e0af09a6487003b3b856e0cd3f69604
Igor Suchilov <59040634+igorsuchilov@users.noreply.github.com>
2021-05-09 14:30:51 +0200

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "terraform-state-test-lab"
-#     key            = "global/s3/terraform.tfstate"
-#     region         = "eu-central-1"
-#     dynamodb_table = "terraform-locks"
-#     encrypt        = true
-#   }
-# }
