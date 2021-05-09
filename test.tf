0ea90439de1e85eaaa154064221506d83fec52ae
Igor Suchilov <59040634+igorsuchilov@users.noreply.github.com>
2021-05-09 15:30:56 +0200

    Update test.tf

--- test.tf
+++ test.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 # Create VPC
 resource "aws_vpc" "main" {
-  cidr_block                     = "172.16.0.0/16"
+  cidr_block                     = "172.18.0.0/16"
   enable_dns_support             = "true"
   enable_dns_hostnames           = "true"
   enable_classiclink             = "false"
